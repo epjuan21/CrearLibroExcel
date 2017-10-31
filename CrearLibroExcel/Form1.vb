440b09de9e10a033afcaa3d9c34ba9d4ab419d7a
Juan Ramirez <ep.juan@gmail.com>
2017-10-31 18:02:05 -0500

    Primera Subida

--- CrearLibroExcel/Form1.vb
+++ CrearLibroExcel/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ExcelApp = New Microsoft.Office.Interop.Excel.Application
+        Dim Libro = ExcelApp.Workbooks.Add
 
+
+        Libro.Sheets(1).Cells(1, 1) = "Hola Mundo"
+
+        Libro.SaveAs(Filename:="C:\Users\USER-XPS\Desktop\Prueba1.xlsx")
+
+        Label1.Text = "Correcto"
+
+        ExcelApp.Quit()
+        Libro = Nothing
+        ExcelApp = Nothing
+
+    End Sub
 End Class
